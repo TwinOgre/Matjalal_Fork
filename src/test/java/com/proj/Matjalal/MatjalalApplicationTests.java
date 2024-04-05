1acf4eb7a826672212e927dc90138bb89b8b0bc6
TwinOgre <glad2you@naver.com>
2024-04-05 16:17:32 +0900

    first commit

---
+++ src/test/java/com/proj/Matjalal/MatjalalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.Matjalal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatjalalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
