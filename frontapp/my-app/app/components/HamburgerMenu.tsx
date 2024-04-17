53e59e8daba3a88ebd2dce13c52e0a8358b15a8d
wintersky <148525670+wintersky2@users.noreply.github.com>
2024-04-17 15:11:31 +0900

    sidebar menu disappear, home link added (#24)

--- frontapp/my-app/app/components/HamburgerMenu.tsx
+++ frontapp/my-app/app/components/HamburgerMenu.tsx
@@ -1,15 +1,33 @@
 'use client'
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function HamburgerMenu() {
     const [sidebarOpen, setSidebarOpen] = useState(false);
+    const sidebarRef = useRef(null);
 
     // 사이드바 토글 함수
     const toggleSidebar = () => {
         setSidebarOpen(!sidebarOpen);
     };
+
+    useEffect(() => {
+        const handleClick = (event) => {
+            // HTML 또는 body를 클릭했을 때 발생하는 이벤트 처리
+            setSidebarOpen(false);
+        };
+
+        // HTML 또는 body에 이벤트 리스너 추가
+        document.documentElement.addEventListener('click', handleClick);
+        document.body.addEventListener('click', handleClick);
+
+        // 컴포넌트가 언마운트될 때 이벤트 리스너 제거
+        return () => {
+            document.documentElement.removeEventListener('click', handleClick);
+            document.body.removeEventListener('click', handleClick);
+        };
+    }, []);
 
     return (
         <div className="flex">
@@ -20,7 +38,7 @@ export default function HamburgerMenu() {
                     <img className="size-10" src="/menu-icon.svg" alt="Menu Icon Image" />
                 )}
             </button>
-            <div className={`fixed lg:w-64 bg-gray-800 ${sidebarOpen ? 'left-0' : '-left-64'} mt-12 transition-all opacity-100 duration-300 ease-in-out`}
+            <div ref={sidebarRef} className={`fixed lg:w-64 bg-gray-800 ${sidebarOpen ? 'left-0' : '-left-64'} mt-12 transition-all opacity-100 duration-300 ease-in-out`}
                 style={{ height: '100vh' }}>
                 <div className='flex flex-col'>
                     <div className="flex justify-center">
