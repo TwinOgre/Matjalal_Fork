8bf04a2d90ee20bcff5ece112f7e18bb4fcdb85b
TwinOgre <glad2you@naver.com>
2024-04-17 15:12:06 +0900

    detail/16

--- frontapp/my-app/app/components/Detail.tsx
+++ frontapp/my-app/app/components/Detail.tsx
@@ -1,24 +1,67 @@
 
 const Detail = () => {
     return (
-        <>
-            <div className="container px-5 py-24 mx-auto">
-                <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-                    ⚡ 제목 
-                    </h1>
-                    <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
-                        작가 <span> | </span><span>생성일</span><span> | </span><span> 수정일 </span>
-                    </p>
-                </div>
-                <div className="flex flex-wrap -m-4">
-                    (재료컴포넌트_재료타입A : 재료1, 재료2 )
-                </div>
-                <button className="flex mx-auto mt-16 text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg">
-                    일단 버튼
-                </button>
-            </div>
-        </>
+<section className="text-gray-600 body-font overflow-hidden">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="lg:w-4/5 mx-auto flex flex-wrap">
+      <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+        <h2 className="text-sm title-font text-gray-500 tracking-widest">
+          SUBWAY
+        </h2>
+        <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
+          Subway 게시글 제목
+        </h1>
+        <div className="flex mb-4">
+          <a className="flex-grow text-green-500 border-b-2 border-green-500 py-2 text-lg px-1">
+            Description
+          </a>
+        </div>
+        <p className="leading-relaxed mb-4">
+          세계 최고의 샌드위치 만드는 법
+        </p>
+        {/* 반복시키기 */}
+        <div className="flex border-t border-gray-200 py-2">
+          <span className="text-gray-500">재료 타입</span>
+          <span className="ml-auto text-gray-900">재료명 <span>|</span></span>
+        </div>
+        <div className="flex border-t border-gray-200 py-2">
+          <span className="text-gray-500">Size</span>
+          <span className="ml-auto text-gray-900">Medium</span>
+        </div>
+        <div className="flex border-t border-b mb-6 border-gray-200 py-2">
+          <span className="text-gray-500">Quantity</span>
+          <span className="ml-auto text-gray-900">4</span>
+        </div>
+        <div className="flex">
+          <span className="title-font font-medium text-2xl text-gray-900">
+            $58.00
+          </span>
+          <button className="flex ml-auto text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded">
+            Button
+          </button>
+          <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+            <svg
+              fill="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <img
+        alt="ecommerce"
+        className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+        src="https://dummyimage.com/400x400"
+      />
+    </div>
+  </div>
+</section>
+
     )
 }
 
